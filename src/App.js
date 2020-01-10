4b4fbf82bbeac7d03beae98fcba7276d8194d687
Jimi Pajala <jimipajala@hotmail.com>
2020-01-10 12:32:00 +0200

    make first version of redoc generated Vehiclelist API documentation

--- src/App.js
+++ src/App.js
@@ -1,26 +1,14 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+
+import { RedocStandalone } from 'redoc';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    <RedocStandalone
+      specUrl="/shareit-vehiclelist-v1-openapi.yaml"
+    />
+  )
 }
 
 export default App;
