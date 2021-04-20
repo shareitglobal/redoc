0f0bb696039f3a2d61e33987a1e3c8bad861a3b8
Jimi Pajala <jimipajala@hotmail.com>
2021-04-20 16:25:16 +0300

    update to correct specification path for Redoc app

--- src/App.js
+++ src/App.js
@@ -6,8 +6,7 @@ import { RedocStandalone } from 'redoc';
 function App() {
   return (
     <RedocStandalone
-      // specUrl={process.env.PUBLIC_URL + "/shareit-vehiclelist-v1-openapi.yaml"}
-      specUrl={process.env.PUBLIC_URL + "/swagger-specification.yaml"}
+      specUrl={process.env.PUBLIC_URL + "/swagger-specification.json"}
     />
   )
 }
