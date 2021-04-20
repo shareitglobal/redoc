a0f6ce84f31bcbdbd82907e178c0af74d5c427d1
Jimi Pajala <jimipajala@hotmail.com>
2021-04-20 16:19:36 +0300

    Update Redoc specification with latest Swagger-schema of reservations
    Co-authored-by: Minh Hoang <minh.hn2016@gmail.com>

--- src/App.js
+++ src/App.js
@@ -6,7 +6,8 @@ import { RedocStandalone } from 'redoc';
 function App() {
   return (
     <RedocStandalone
-      specUrl={process.env.PUBLIC_URL + "/shareit-vehiclelist-v1-openapi.yaml"}
+      // specUrl={process.env.PUBLIC_URL + "/shareit-vehiclelist-v1-openapi.yaml"}
+      specUrl={process.env.PUBLIC_URL + "/swagger-specification.yaml"}
     />
   )
 }
